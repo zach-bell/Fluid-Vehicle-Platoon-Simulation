5da5d7881c844ad9cad163938d52e5cc08b3a147
Zachary Vanscoit <zachary816@gmail.com>
2019-04-25 23:50:07 -0500

    updated particle handler

--- PlatoonSimulation/src/core/ParticleHandler.java
+++ PlatoonSimulation/src/core/ParticleHandler.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.util.ArrayList;
+
 import core.entity.Particle;
 import processing.core.PApplet;
 import processing.core.PVector;
@@ -7,6 +9,7 @@ import processing.core.PVector;
 public class ParticleHandler {
 	
 	private Particle[] particleList;
+	private ArrayList<ArrayList<Particle>> particleListOverField;
 	private PApplet launcher;
 	private NoiseFlowField flowField;
 	
@@ -14,7 +17,8 @@ public class ParticleHandler {
 		this.launcher = launcher;
 		this.flowField = flowField;
 		
-		particleList = new Particle[1000];
+		particleList = new Particle[750];
+		particleListOverField = new ArrayList<ArrayList<Particle>>(flowField.cols * flowField.rows);
 		initParticles();
 	}
 	
@@ -29,6 +33,19 @@ public class ParticleHandler {
 		for (Particle p : particleList) {
 			p.update();
 			p.draw();
+		}
+	}
+	
+	public void updateOverField() {
+		for (ArrayList<Particle> aPlist : particleListOverField) {
+			aPlist.clear();
+		}
+		for (Particle p : particleList) {
+			int x = PApplet.floor(p.position.x / flowField.scale);
+			int y = PApplet.floor(p.position.y / flowField.scale);
+			int index = x + y * flowField.cols;
+			index = PApplet.constrain(index, 1, flowField.cols * flowField.rows) - 1;
+			particleListOverField.get(index).add(p);
 		}
 	}
 }
